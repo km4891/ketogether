beb460f33d25457e9bef4302732d571af2e12d5f
km4891 <kmorarity@gmail.com>
2021-02-03 17:46:21 -0800

    spoonacular

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CategoryMenu from "../components/CategoryMenu";
-import RecipeList from "../components/RecipeList";
+import RecipeList from "../components/UserDashboard";
+import AddRecipe from "../components/AddRecipe";
 
 const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
       <RecipeList />
+      <AddRecipe />
     </div>
   );
 };
